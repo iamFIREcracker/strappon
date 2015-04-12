854169c3f8f5eaa47da61779fc6967506ccf2b90
Matteo Landi <matteo@matteolandi.net>
2015-04-12 10:28:41 -0400

    pylint, bitches

--- strappon/repositories/drive_requests.py
+++ strappon/repositories/drive_requests.py
@@ -10,6 +10,7 @@ from strappon.models import Passenger
 from strappon.models import Rate
 from strappon.models import User
 from sqlalchemy.sql.expression import true
+from sqlalchemy.sql.expression import false
 from weblib.db import and_
 from weblib.db import exists
 from weblib.db import expunged
@@ -22,14 +23,14 @@ class DriveRequestsRepository(object):
     def get_unrated_by_id(id, driver_id, user_id):
         options = [joinedload_all('driver.user'),
                    joinedload_all('passenger.user')]
-        return expunged(DriveRequest.query.options(*options).\
-                         filter(DriveRequest.driver_id == driver_id).\
-                         filter(DriveRequest.accepted == True).\
-                         filter(DriveRequest.cancelled == False).\
-                         filter(DriveRequest.active == False).\
-                         filter(Passenger.matched == True).\
-                         filter(~exists().where(and_(Rate.drive_request_id == DriveRequest.id,
-                                                      Rate.rater_user_id == user_id))).\
+        return expunged(DriveRequest.query.options(*options).
+                        filter(DriveRequest.driver_id == driver_id).
+                        filter(DriveRequest.accepted == true()).
+                        filter(DriveRequest.cancelled == false()).
+                        filter(DriveRequest.active == false()).
+                        filter(Passenger.matched == true()).
+                        filter(~exists().where(and_(Rate.drive_request_id == DriveRequest.id,
+                                                    Rate.rater_user_id == user_id))).
                         first(),
                         DriveRequest.session)
 
@@ -38,42 +39,41 @@ class DriveRequestsRepository(object):
         options = [joinedload_all('driver.user'),
                    joinedload_all('passenger.user')]
         return [expunged(dr, DriveRequest.session)
-                for dr in DriveRequest.query.options(*options).\
-                         filter(DriveRequest.driver_id == driver_id).\
-                         filter(DriveRequest.accepted == True).\
-                         filter(DriveRequest.cancelled == False).\
-                         filter(DriveRequest.active == False).\
-                         filter(Passenger.matched == True).\
-                         filter(~exists().where(and_(Rate.drive_request_id == DriveRequest.id,
-                                                      Rate.rater_user_id == user_id)))
-                ]
+                for dr in DriveRequest.query.options(*options).
+                filter(DriveRequest.driver_id == driver_id).
+                filter(DriveRequest.accepted == true()).
+                filter(DriveRequest.cancelled == false()).
+                filter(DriveRequest.active == false()).
+                filter(Passenger.matched == true()).
+                filter(~exists().where(and_(Rate.drive_request_id == DriveRequest.id,
+                                            Rate.rater_user_id == user_id)))]
 
     @staticmethod
     def get_all_active():
         return [expunged(dr, DriveRequest.session)
-                for dr in DriveRequest.query.\
-                        filter(User.deleted == False).\
-                        filter(DriveRequest.active == True)]
+                for dr in DriveRequest.query.
+                filter(User.deleted == false()).
+                filter(DriveRequest.active == true())]
 
     @staticmethod
     def get_all_active_by_driver(driver_id):
         options = [joinedload_all('driver.user'),
                    joinedload_all('passenger.user')]
         return [expunged(dr, DriveRequest.session)
-                for dr in DriveRequest.query.options(*options).\
-                        filter(User.deleted == False).\
-                        filter(DriveRequest.driver_id == driver_id).\
-                        filter(DriveRequest.active == True)]
+                for dr in DriveRequest.query.options(*options).
+                filter(User.deleted == false()).
+                filter(DriveRequest.driver_id == driver_id).
+                filter(DriveRequest.active == true())]
 
     @staticmethod
     def get_all_active_by_passenger(passenger_id):
         options = [joinedload_all('driver.user'),
                    joinedload_all('passenger.user')]
         return [expunged(dr, DriveRequest.session)
-                for dr in DriveRequest.query.options(*options).\
-                        filter(User.deleted == False).\
-                        filter(DriveRequest.passenger_id == passenger_id).\
-                        filter(DriveRequest.active == True)]
+                for dr in DriveRequest.query.options(*options).
+                filter(User.deleted == false()).
+                filter(DriveRequest.passenger_id == passenger_id).
+                filter(DriveRequest.active == true())]
 
     @staticmethod
     def add(driver_id, passenger_id, offered_pickup_time):
@@ -99,24 +99,23 @@ class DriveRequestsRepository(object):
 
     @staticmethod
     def cancel_by_passenger_id(id, passenger_id):
-        request = expunged(DriveRequest.query.\
-                                filter_by(id=id).\
-                                filter_by(passenger_id=passenger_id).\
-                                filter_by(active=True).first(),
+        request = expunged(DriveRequest.query.
+                           filter_by(id=id).
+                           filter_by(passenger_id=passenger_id).
+                           filter_by(active=True).first(),
                            DriveRequest.session)
         if request:
             request.active = False
             request.cancelled = True
         return request
 
-
     @staticmethod
     def accept(driver_id, passenger_id):
-        request = expunged(DriveRequest.query.\
-                                filter_by(driver_id=driver_id).\
-                                filter_by(passenger_id=passenger_id).\
-                                filter_by(accepted=False).\
-                                filter_by(active=True).first(),
+        request = expunged(DriveRequest.query.
+                           filter_by(driver_id=driver_id).
+                           filter_by(passenger_id=passenger_id).
+                           filter_by(accepted=False).
+                           filter_by(active=True).first(),
                            DriveRequest.session)
         if request:
             request.accepted = True
